c2f626137c2ea02c8ba1e570cd6442f0fd7d12d8
maclmatt <mayaclmatthews@gmail.com>
2023-11-09 16:17:24 +0000

    Add decknumber and getDeckNumber to deck class

--- src/main/java/org/example/Deck.java
+++ src/main/java/org/example/Deck.java
@@ -6,21 +6,26 @@ import java.util.Queue;
 
 public class Deck {
     private final Queue<Card> cards;
+    private final int decknumber;
 
     /**
      * Constructs an empty deck of cards
+     * @param dn the number of the deck
      */
-    public Deck() {
+    public Deck(int dn) {
         cards = new ConcurrentLinkedQueue<>();
+        decknumber = dn;
     }
 
     /**
      * Constructs a deck containing a given collection of cards
      * @param initial_cards The cards the deck will be initialized using
+     * @param dn the number of the deck
      */
-    public Deck(Collection<Card> initial_cards) {
+    public Deck(Collection<Card> initial_cards, int dn) {
         cards = new ConcurrentLinkedQueue<>();
         cards.addAll(initial_cards);
+        decknumber = dn;
     }
 
     /**
@@ -37,5 +42,13 @@ public class Deck {
      */
     public Card dealNextCard() {
         return cards.poll();
+    }
+
+    /**
+     * Returns the number of the deck
+     * @return deck number
+     */
+    public int getDeckNumber() {
+        return decknumber;
     }
 }
