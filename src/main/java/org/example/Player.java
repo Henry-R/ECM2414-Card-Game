632354ec039100e8360ca05e29eba61f8cdf84cc
maclmatt <mayaclmatthews@gmail.com>
2023-11-09 15:19:12 +0000

    drawCard change, to account for preffered values

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -3,6 +3,7 @@ import java.util.Queue;
 import java.util.ArrayDeque;
 
 public class Player {
+    private final int preferredDenom;
     private final Deck inputdeck;
     private final Deck outputdeck;
     private Queue<Card> hand;
@@ -10,11 +11,13 @@ public class Player {
 
     /**
      * Constructs the Player with a given input deck, output deck and initial hand
+     * @param pd The preferred denomination of cards for the player
      * @param id The deck for the player to draw cards from
      * @param od The deck for the player to discard cards to
      * @param h The initial hand of cards for the player
      */
-    public Player(Deck id, Deck od, Queue<Card> h) {
+    public Player(int pd, Deck id, Deck od, Queue<Card> h) {
+        preferredDenom = pd;
         inputdeck = id;
         outputdeck = od;
         hand = h;
@@ -22,13 +25,12 @@ public class Player {
     }
 
     /**
-     * Draws card from input deck and adds to hand
+     * Draws card from input deck
      * Returns the denomination of the drawn card
      * @return newly drawn card's denomination
      */
     public int drawCard() {
         Card drawnCard = inputdeck.dealNextCard();
-        hand.add(drawnCard);
         return drawnCard.getDenomination();
     }
 
