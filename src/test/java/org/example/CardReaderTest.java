b2b31ece85e14ffedcc22278802a8d4e15c63593
Henry Wilson <hrw221@exeter.ac.uk>
2023-11-09 00:41:46 +0000

    moved CardReader test files to resources

--- src/test/java/org/example/CardReaderTest.java
+++ src/test/java/org/example/CardReaderTest.java
@@ -5,23 +5,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardReaderTest {
-    private final String PATH = "C:\\Users\\henry\\IdeaProjects\\CardGame\\src\\test\\java\\org\\example\\";
-
     @Test
     void success() {
-        var reader = new CardReader(PATH + "test_deck.txt");
+        final var URL = this.getClass().getResource("/test_deck.txt");
+        assertNotNull(URL);
+
+        var reader = new CardReader(URL.getPath());
         assertTrue(reader.success());
     }
 
     @Test
     void getCards() {
-        var reader1 = new CardReader(PATH + "test_deck.txt");
+        final var test1URL = this.getClass().getResource("/test_deck.txt");
+        assertNotNull(test1URL);
+
+        var reader1 = new CardReader(test1URL.getPath());
         var cards1 = reader1.getCards();
         assertEquals(1, cards1.get(0).getDenomination());
         assertEquals(2, cards1.get(1).getDenomination());
         assertEquals(3, cards1.get(2).getDenomination());
 
-        var reader2 = new CardReader(PATH + "test_bad_deck.txt");
+        final var test2URL = this.getClass().getResource("/test_bad_deck.txt");
+        assertNotNull(test2URL);
+        var reader2 = new CardReader(test2URL.getPath());
         var cards2 = reader2.getCards();
         assertEquals(1, cards2.get(0).getDenomination());
         assertEquals(2, cards2.get(1).getDenomination());
