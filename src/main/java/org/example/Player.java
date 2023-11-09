eb3c91194ced123b29a57d930e697344c0664744
maclmatt <mayaclmatthews@gmail.com>
2023-11-09 15:03:57 +0000

    Player class constructor

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,7 +1,21 @@
 package org.example;
+import java.util.Queue;
+import java.util.ArrayDeque;
 
 public class Player {
-    public Player() {
+    private final Deck inputdeck;
+    private final Deck outputdeck;
+    private Queue<Card> hand;
 
+    /**
+     * Constructs the Player with a given input deck, output deck and initial hand
+     * @param id The deck for the player to draw cards from
+     * @param od The deck for the player to discard cards to
+     * @param h The initial hand of cards for the player
+     */
+    public Player(Deck id, Deck od, Queue<Card> h) {
+        inputdeck = id;
+        outputdeck = od;
+        hand = h;
     }
 }
