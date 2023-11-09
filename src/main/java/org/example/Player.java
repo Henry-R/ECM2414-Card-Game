1b776a96881fc72caab5dcc30dfd5d84bdc8f727
maclmatt <mayaclmatthews@gmail.com>
2023-11-09 15:15:17 +0000

    drawCard and discardCard methods for Player class

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -6,6 +6,7 @@ public class Player {
     private final Deck inputdeck;
     private final Deck outputdeck;
     private Queue<Card> hand;
+    private int preferredcount;
 
     /**
      * Constructs the Player with a given input deck, output deck and initial hand
@@ -17,5 +18,30 @@ public class Player {
         inputdeck = id;
         outputdeck = od;
         hand = h;
+        preferredcount = 0;
     }
+
+    /**
+     * Draws card from input deck and adds to hand
+     * Returns the denomination of the drawn card
+     * @return newly drawn card's denomination
+     */
+    public int drawCard() {
+        Card drawnCard = inputdeck.dealNextCard();
+        hand.add(drawnCard);
+        return drawnCard.getDenomination();
+    }
+
+    /**
+     * Discards card from hand and adds to output deck
+     * Returns denomination of the discarded card
+     * @return newly discarded card's denomination
+     */
+    public int discardCard() {
+        Card discardedCard = hand.remove();
+        outputdeck.pushCard(discardedCard);
+        return discardedCard.getDenomination();
+    }
+
+
 }
