9e4a4520c29ad6045d88d4406fa00324d3eee2d5
Henry Wilson <hrw221@exeter.ac.uk>
2023-11-16 18:29:14 +0000

    Added comments and docstrings to all of PlayerJudge's methods

--- src/main/java/player/PlayerJudge.java
+++ src/main/java/player/PlayerJudge.java
@@ -1,26 +1,52 @@
 package player;
 
+/**
+ * Acts as shared memory for Player objects. Player objects use this class to communicate when a player has won, and
+ * how many turns it took them. This allows the Player objects to stop if they know they cannot win
+ */
 public class PlayerJudge {
+    // Turn the current winner won on
     int winningTurn;
+    // Player number of the currently winning player
     int winningPlayer;
 
+    /**
+     * Constructs an instance of PlayerJudge class where no players have already won
+     */
     public PlayerJudge() {
-        winningTurn = 0;
+        winningTurn = -1;
         winningPlayer = -1;
     }
 
+    /**
+     * Declares a new winner. If the new winner has won sooner than the old winner, update to the new winner
+     * @param playerID Player number of the new winner
+     * @param turn Turn the winner won on
+     */
     public synchronized void newWinner(int playerID, int turn) {
         if (turn < winningTurn || !playerHasWon()) {
             winningTurn = turn;
             winningPlayer = playerID;
         }
     }
+
+    /**
+     * @return True is a player has won, false otherwise
+     */
     public synchronized boolean playerHasWon() {
         return winningPlayer != -1;
     }
+
+    /**
+     * @return The turn the currently winning player won on, -1 if no player has won yet
+     */
     public synchronized int getWinningTurn() {
         return winningTurn;
     }
+
+    /**
+     * @return Player number of the currently winning player, -1 if no player has won yet
+     */
     public synchronized int getWinningPlayer() {
         return winningPlayer;
     }
