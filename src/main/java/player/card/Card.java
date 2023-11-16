e61f9131341c40b6d37c4170f8aaf0609186955b
Henry Wilson <hrw221@exeter.ac.uk>
2023-11-16 18:40:52 +0000

    Added comments and docstrings to all of Card's methods

--- src/main/java/player/card/Card.java
+++ src/main/java/player/card/Card.java
@@ -1,14 +1,18 @@
 package player.card;
 
+/**
+ * A wrapper for a Card's denomination. Provides a safe read-only abstraction for the denomination integer value
+ */
 public class Card {
+    // Value of this card
     private final int denomination;
 
     /**
      * Constructs the card with a given denominator
-     * @param d The denominator of the card
+     * @param denomination The denominator of the card
      */
-    public Card(int d) {
-        denomination = d;
+    public Card(int denomination) {
+        this.denomination = denomination;
     }
 
     /**
